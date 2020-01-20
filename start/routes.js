e08c97cecc22efc39af3b352e8df09cea8232896
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2020-01-20 16:25:54 -0300

    refactor: routes made automatically by adonis

--- start/routes.js
+++ start/routes.js
@@ -3,8 +3,19 @@ const Route = use('Route');
 
 Route.post('/users', 'UserController.create').validator('User');
 Route.post('/login', 'SessionController.create').validator('Session');
-Route.get('/task', 'TaskController.index').middleware(['auth']);
-Route.post('/task', 'TaskController.store').middleware(['auth']).validator('Task');
-Route.get('/task/:id', 'TaskController.show').middleware(['auth']);
-Route.put('/task/:id', 'TaskController.update').middleware(['auth']).validator('Task');
-Route.delete('/task/:id', 'TaskController.destroy').middleware(['auth']);
+
+Route.group(() => {
+  Route.resource('task', 'TaskController')
+    .validator(new Map(
+      [
+        [
+          ['task.store'],
+          ['Task'],
+        ], [
+          ['task.update'],
+          ['Task'],
+        ],
+      ],
+    ))
+    .apiOnly();
+}).middleware(['auth']);
